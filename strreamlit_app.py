a7a0a244a5bea95134c6a0797bcd7799534b5d00
senafielCCL <Senafielccl@gmail.com>
2026-05-09 23:45:26 +0700

    Update strreamlit_app.py

--- strreamlit_app.py
+++ strreamlit_app.py
@@ -270,10 +270,10 @@ with st.sidebar:
         st.rerun()
 
 # Header utama (juga dibungkus card putih transparan)
-st.markdown('<div class="main-card">', unsafe_allow_html=True)
+
 st.markdown(f"## 👋 Selamat datang, {st.session_state.username}")
 st.markdown(f"{datetime.now().strftime('%A, %d %B %Y')}")
-st.markdown('</div>', unsafe_allow_html=True)
+
 
 # ==================== 5. DASHBOARD ====================
 role = st.session_state.role
