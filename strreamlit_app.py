c16b852151fec402ac61ba8291dc695b447402ab
BagusIPH <onaajahp@gmail.com>
2026-05-09 15:45:35 +0700

    Update strreamlit_app.py

--- strreamlit_app.py
+++ strreamlit_app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import base64
 
 # ==================== 0. KONFIGURASI HALAMAN ====================
-st.set_page_config(page_title="MDMS - CV Amal Mulia", layout="wide", page_icon="🏭")
+st.set_page_config(page_title="90S - CV Amal Mulia", layout="wide", page_icon="🏭")
 
 # -------------------- FUNGSI UNTUK BACKGROUND GIF (BERGERAK) --------------------
 import streamlit as st
