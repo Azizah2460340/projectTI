84f96a3d352528c52be600fe608790b11c25d76f
BagusIPH <onaajahp@gmail.com>
2026-05-09 13:40:48 +0700

    Fix import statement for plotly.express

--- strreamlit_app.py
+++ strreamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 from datetime import datetime
-import plotly.express as px
+
 import base64
 
 # ==================== 0. KONFIGURASI HALAMAN ====================
