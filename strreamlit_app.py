2765d740797238c75f25def2ac8025d9d3e0aaaa
senafielCCL <Senafielccl@gmail.com>
2026-05-09 23:27:26 +0700

    Update strreamlit_app.py

--- strreamlit_app.py
+++ strreamlit_app.py
@@ -273,7 +273,7 @@ with st.sidebar:
 st.markdown('<div class="main-card">', unsafe_allow_html=True)
 st.markdown(f"## 👋 Selamat datang, {st.session_state.username}")
 st.markdown(f"{datetime.now().strftime('%A, %d %B %Y')}")
-st.markdown('</div>', unsafe_allow_html=True)
+
 
 # ==================== 5. DASHBOARD ====================
 role = st.session_state.role
