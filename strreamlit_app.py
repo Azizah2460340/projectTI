03952974e507f4eed86f1656eb3b61c54752e98a
senafielCCL <Senafielccl@gmail.com>
2026-05-09 23:13:11 +0700

    Update strreamlit_app.py

--- strreamlit_app.py
+++ strreamlit_app.py
@@ -4,11 +4,11 @@ import sqlite3
 from datetime import datetime
 import plotly.express as px
 import base64
-from PIL import Image
+
 
 # ==================== 0. KONFIGURASI HALAMAN ====================
-logo_cv = Image.open("LogoCV.jpeg")
-st.set_page_config(page_title = "MDMS - CV Amal Mulia", layout="wide", page_icon="LogoCV")
+
+st.set_page_config(page_title = "MDMS - CV Amal Mulia", layout="wide", page_icon="🏭")
 
 # -------------------- FUNGSI UNTUK BACKGROUND GIF (BERGERAK) --------------------
 import streamlit as st
