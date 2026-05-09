695704097af2a8b02ebe2b487cc97dc3e5f2cb15
senafielCCL <Senafielccl@gmail.com>
2026-05-09 23:39:14 +0700

    Update strreamlit_app.py

--- strreamlit_app.py
+++ strreamlit_app.py
@@ -264,7 +264,7 @@ if not st.session_state.authenticated:
 with st.sidebar:
     st.markdown("### 🏭 MDMS")
     st.markdown(f"**👤 {st.session_state.username}**")
-    st.markdown(f"**Role:** `{st.session_state.role.upper()}`")
+    st.markdown(f"**Role:** `{st.session_state.role.upper()}`", use_container_width=True)
     if st.button("🚪 Logout", use_container_width=True):
         st.session_state.authenticated = False
         st.rerun()
